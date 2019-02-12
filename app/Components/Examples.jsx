e4265b108ea72d254a39d05044eb7815c7eead0a
Gyenes Zsolt Márk <sociopathyasuo@Gyenes-MacBook-Pro.local>
2019-02-12 15:01:15 +0100

    Added copy

--- app/Components/Examples.jsx
+++ app/Components/Examples.jsx
@@ -1,7 +1,11 @@
 var React = require('react');
 
 var Examples = (props) => {
-  return (<h3>Examples component!</h3>)
+  return (
+    <div>
+      <h3>Examples component!</h3>
+      <p>Welcome to examples page!</p>
+    </div>)
 
 };
 
